2472fc96d73b70f810868123874500cc5eb5c964
Nguyen Tran <nguyen_tran@datahouse.com>
2023-09-01 23:43:29 +0700

    feat: add scraping serverless

--- src/modules/room/application/createRoom/createRoom.handler.ts
+++ src/modules/room/application/createRoom/createRoom.handler.ts
@@ -1,7 +1,8 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { CreateRoomCommand } from './createRoom.command';
-import { BadRequestException } from '@nestjs/common';
 import { PrismaService } from '@/common/database';
+import { BadRequestException } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import fetch from 'node-fetch';
+import { CreateRoomCommand } from './createRoom.command';
 
 @CommandHandler(CreateRoomCommand)
 export class CreateRoomHandler implements ICommandHandler<CreateRoomCommand, void> {
@@ -13,23 +14,29 @@ export class CreateRoomHandler implements ICommandHandler<CreateRoomCommand, voi
       body: { scrapingUrl, dueTime, alias },
     } = command;
 
-    const scraped = { data: {}, json: '' };
+    const apiUrl = new URL('https://ot-s.onrender.com/api/scraping');
+    apiUrl.searchParams.set('url', encodeURI(scrapingUrl));
+    try {
+      const response = await fetch(apiUrl.href);
+      const scraped = await response.json();
 
-    if (!scraped) {
-      throw new BadRequestException('The restaurant url is invalid.');
+      if (!scraped) {
+        throw new BadRequestException('The restaurant url is invalid.');
+      }
+
+      await this.db.room.create({
+        data: {
+          hostId: reqUser.id,
+          scrapingUrl,
+          alias,
+          scrapingData: JSON.stringify(scraped),
+          dueTime,
+        },
+      });
+
+      return;
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
-
-    const { json } = scraped;
-    await this.db.room.create({
-      data: {
-        hostId: reqUser.id,
-        scrapingUrl,
-        alias,
-        scrapingData: json,
-        dueTime,
-      },
-    });
-
-    return;
   }
 }
