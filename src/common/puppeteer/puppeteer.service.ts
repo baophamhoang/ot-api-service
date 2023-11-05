300c9eb34155d3733339daf65b96d816b6bbc057
Bao Pham <bao_pham@datahouse.com>
2023-11-04 19:14:38 -1000

    feat: add puppeteer config

--- src/common/puppeteer/puppeteer.service.ts
+++ src/common/puppeteer/puppeteer.service.ts
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import { ConfigService } from '@/configs';
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ScrapeRestaurantResponse } from './interface';
+import os from 'os';
+import fs from 'fs';
 
 @Injectable()
 export class PuppeteerService implements OnModuleInit {
@@ -36,13 +38,101 @@ export class PuppeteerService implements OnModuleInit {
 }
 
 const getOptions = async (config: ConfigService): Promise<Parameters<typeof puppeteer.launch>['0']> => {
+  // Get the home directory
+  const homeDirectory = os.homedir();
+
   const exePath =
     process.platform === 'win32'
       ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
       : process.platform === 'linux'
-      ? '/usr/bin/google-chrome'
+      ? '/usr/bin/chromium-browser'
       : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  const options = { args: [], executablePath: exePath };
+
+  const options = {
+    args: [],
+    executablePath: exePath,
+  };
+
+  fs.readdir(homeDirectory, (err, files) => {
+    if (err) {
+      console.error('Error reading cache directory:', err);
+    } else {
+      files.forEach((file) => {
+        console.log(file);
+      });
+    }
+  });
+
+  fs.readdir(`${homeDirectory}/project`, (err, files) => {
+    if (err) {
+      console.error('Error reading project directory:', err);
+    } else {
+      console.log('-> start reading project: ');
+      files.forEach((file) => {
+        console.log('+ ', file);
+      });
+      console.log('-> end reading:');
+    }
+  });
+
+  fs.readdir(`${homeDirectory}/project/src`, (err, files) => {
+    if (err) {
+      console.error('Error reading src directory:', err);
+    } else {
+      console.log('-> stazc:  ');
+      files.forEach((file) => {
+        console.log('+ ', file);
+      });
+      console.log('-> end reading: ');
+    }
+  });
+
+  fs.readdir(`${homeDirectory}/.cache`, (err, files) => {
+    if (err) {
+      console.error('Error reading src directory:', err);
+    } else {
+      console.log('-> start reading cache:  ');
+      files.forEach((file) => {
+        console.log('+ ', file);
+      });
+      console.log('-> end reading: ');
+    }
+  });
+
+  fs.readdir(`${homeDirectory}/.cache/puppeteer`, (err, files) => {
+    if (err) {
+      console.error('Error reading src/puppeteer directory:', err);
+    } else {
+      console.log('-> start reading cache:  ');
+      files.forEach((file) => {
+        console.log('+ ', file);
+      });
+      console.log('-> end reading: ');
+    }
+  });
+
+  // readDir(homeDirectory);
 
   return { ...options, headless: true };
+
+  return {
+    // ...options,
+    headless: false,
+    args: ['--no-sandbox'],
+  };
+};
+
+const readDir = (directory: string) => {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error('Error reading cache directory:', err);
+    } else {
+      console.log('-> start reading: ', directory);
+      files.forEach((file) => {
+        console.log('+ ', file);
+        readDir(file);
+      });
+      console.log('-> end reading: ', directory);
+    }
+  });
 };
